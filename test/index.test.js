d70d119e7ccff7bf282ff3b69aa7f24ee980c9ba
Andre Staltz <andre@staltz.com>
2023-07-18 10:39:54 +0300

    add identityAdd API

--- test/index.test.js
+++ test/index.test.js
@@ -15,25 +15,24 @@ function setup() {
   rimraf.sync(path)
   const keypair = Keypair.generate('ed25519', 'alice')
 
-  const stack = require('secret-stack/lib/api')([], {})
-    .use(require('secret-stack/lib/core'))
-    .use(require('secret-stack/lib/plugins/net'))
+  const local = require('secret-stack/bare')({ caps })
+    .use(require('secret-stack/plugins/net'))
     .use(require('secret-handshake-ext/secret-stack'))
+    .use(require('ppppp-db'))
     .use(require('../lib'))
     .call(null, {
       path,
-      caps,
       keypair,
     })
 
-  return { stack, path, keypair }
+  return { local, path, keypair }
 }
 
 test('create()', async (t) => {
-  const { stack, path } = setup()
+  const { local, path } = setup()
 
   const promise = { type: 'follow' }
-  const token = await p(stack.promise.create)(promise)
+  const token = await p(local.promise.create)(promise)
   assert.strictEqual(typeof token, 'string')
   assert.ok(token.length > 42)
 
@@ -42,46 +41,103 @@ test('create()', async (t) => {
   const contents = fs.readFileSync(file, 'utf-8')
   assert.strictEqual(contents, JSON.stringify([[token, promise]]))
 
-  await p(stack.close)()
+  await p(local.close)()
 })
 
 test('follow()', async (t) => {
-  const { stack, path } = setup()
+  const { local, path } = setup()
 
-  assert.rejects(() => p(stack.promise.follow)('randomnottoken', 'MY_ID'))
+  assert.rejects(() => p(local.promise.follow)('randomnottoken', 'MY_ID'))
 
   const promise = { type: 'follow' }
-  const token = await p(stack.promise.create)(promise)
+  const token = await p(local.promise.create)(promise)
 
   const file = Path.join(path, 'promises.json')
   const contentsBefore = fs.readFileSync(file, 'utf-8')
   assert.strictEqual(contentsBefore, JSON.stringify([[token, promise]]))
 
-  const result1 = await p(stack.promise.follow)(token, 'MY_ID')
+  const result1 = await p(local.promise.follow)(token, 'MY_ID')
   assert.strictEqual(result1, true)
 
   const contentsAfter = fs.readFileSync(file, 'utf-8')
   assert.strictEqual(contentsAfter, '[]')
 
-  assert.rejects(() => p(stack.promise.follow)(token, 'MY_ID'));
+  assert.rejects(() => p(local.promise.follow)(token, 'MY_ID'))
 
-  await p(stack.close)()
+  await p(local.close)()
 })
 
-test('revoke()', async (t) => {
-  const { stack, path } = setup()
+test('identityAdd()', async (t) => {
+  const { local, path, keypair } = setup()
 
-  const promise = { type: 'follow' }
-  const token = await p(stack.promise.create)(promise)
+  assert.rejects(() => p(local.promise.identityAdd)('randomnottoken', {}))
+
+  const identity = await p(local.db.identity.findOrCreate)({
+    domain: 'account',
+  })
+
+  const promise = { type: 'identity-add', identity }
+  const token = await p(local.promise.create)(promise)
 
   const file = Path.join(path, 'promises.json')
   const contentsBefore = fs.readFileSync(file, 'utf-8')
   assert.strictEqual(contentsBefore, JSON.stringify([[token, promise]]))
 
-  await p(stack.promise.revoke)(token)
+  const dbBefore = [...local.db.msgs()].map(({data}) => data)
+  assert.equal(dbBefore.length, 1)
+  assert.equal(dbBefore[0].action, 'add')
+  assert.equal(dbBefore[0].add.key.algorithm, 'ed25519')
+  assert.equal(dbBefore[0].add.key.bytes, keypair.public)
+  assert.equal(dbBefore[0].add.key.purpose, 'sig')
+  assert(dbBefore[0].add.nonce)
+
+  const keypair2 = Keypair.generate('ed25519', 'bob')
+  const consent = local.db.identity.consent({ identity, keypair: keypair2 })
+  const result1 = await p(local.promise.identityAdd)(token, {
+    key: {
+      purpose: 'sig',
+      algorithm: 'ed25519',
+      bytes: keypair2.public,
+    },
+    consent,
+  })
+  assert.strictEqual(result1, true)
+
+  const dbAfter = [...local.db.msgs()].map(({data}) => data)
+  assert.equal(dbAfter.length, 2)
+  assert.equal(dbAfter[0].action, 'add')
+  assert.equal(dbAfter[0].add.key.algorithm, 'ed25519')
+  assert.equal(dbAfter[0].add.key.bytes, keypair.public)
+  assert.equal(dbAfter[0].add.key.purpose, 'sig')
+  assert(dbAfter[0].add.nonce)
+  assert.equal(dbAfter[1].action, 'add')
+  assert.equal(dbAfter[1].add.key.algorithm, 'ed25519')
+  assert.equal(dbAfter[1].add.key.bytes, keypair2.public)
+  assert.equal(dbAfter[1].add.key.purpose, 'sig')
+  assert(dbAfter[1].add.consent)
 
   const contentsAfter = fs.readFileSync(file, 'utf-8')
   assert.strictEqual(contentsAfter, '[]')
 
-  await p(stack.close)()
+  assert.rejects(() => p(local.promise.identityAdd)(token, {}))
+
+  await p(local.close)()
+})
+
+test('revoke()', async (t) => {
+  const { local, path } = setup()
+
+  const promise = { type: 'follow' }
+  const token = await p(local.promise.create)(promise)
+
+  const file = Path.join(path, 'promises.json')
+  const contentsBefore = fs.readFileSync(file, 'utf-8')
+  assert.strictEqual(contentsBefore, JSON.stringify([[token, promise]]))
+
+  await p(local.promise.revoke)(token)
+
+  const contentsAfter = fs.readFileSync(file, 'utf-8')
+  assert.strictEqual(contentsAfter, '[]')
+
+  await p(local.close)()
 })
